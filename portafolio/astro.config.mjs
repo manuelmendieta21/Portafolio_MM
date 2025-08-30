74611eeec2d9d72bfc8d8112e237c9c0fb619c1e
Manuel Andres Mendieta <manuelmendieta21@gmail.com>
2025-08-30 20:09:42 +0200

    create update

--- portafolio/astro.config.mjs
+++ portafolio/astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://mimanudev.site',
   integrations: [
     tailwind()
   ],
