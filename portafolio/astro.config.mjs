fdf8b1df2f64d6ea9022358e8c5e81821a8c043a
Manuel Andres Mendieta <46815715+manuelmendieta21@users.noreply.github.com>
2026-08-27 17:28:54 +0200

    Update astro.config.mjs

--- portafolio/astro.config.mjs
+++ portafolio/astro.config.mjs
@@ -6,7 +6,6 @@ export default defineConfig({
   // astro.config.mjs changes site url dominio
   site: 'https://manuelmendieta21.github.io',
   //change base agree
-  base: '/Portafolio_MM',
   integrations: [
     tailwind()
   ],
