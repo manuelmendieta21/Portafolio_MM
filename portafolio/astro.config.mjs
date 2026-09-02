1433304beef4fef35fb3fbc6e5c5e0fd870a378c
Manuel Mendieta <manuelmendieta21@gmail.com>
2026-09-02 21:44:20 +0200

    Fix GitHub Pages navigation URLs

--- portafolio/astro.config.mjs
+++ portafolio/astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://mimanudev.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/Portafolio_MM' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Portafolio_MM/' : '/',
   integrations: [tailwind()],
 });
 
