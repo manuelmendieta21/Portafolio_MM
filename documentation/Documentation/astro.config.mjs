c7811271f61bbadc1065e52d3472dc3b6b38977e
Manuel Mendieta <manuelmendieta21@gmail.com>
2026-09-02 21:16:15 +0200

    url documentation this proyect aline

--- documentation/Documentation/astro.config.mjs
+++ documentation/Documentation/astro.config.mjs
@@ -4,6 +4,9 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://manuelmendieta21.github.io',
+	base: process.env.NODE_ENV === 'production' ? '/Portafolio_MM/documentation' : '/',
+	server: { port: 4322 },
 	integrations: [
 		starlight({
 			title: 'Documentación 🌟',
